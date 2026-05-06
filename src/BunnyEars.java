84bfffa2e10679e44257b81b5e675917136df23d
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-05-06 15:35:50 +0400

    added recursive Case

--- src/BunnyEars.java
+++ src/BunnyEars.java
@@ -4,7 +4,11 @@ public class BunnyEars {
     public int bunnyEars2(int n) {
         if (n==0){
             return 0;
+        }
+        else {
+            if (n%2==0){
 
+            }
         }
 
     }
