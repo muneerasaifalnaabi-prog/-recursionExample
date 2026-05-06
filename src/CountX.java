508f0856094c37f210116e07078068a848b4bc38
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-05-06 16:07:14 +0400

    Completed CountX class

--- src/CountX.java
+++ src/CountX.java
@@ -7,6 +7,12 @@ public class CountX {
         //Recursive Case
         else{
             char firstChar =str.charAt(0);
+            if (firstChar=='x'){
+                return 1 + countX(str.substring(1));
+            }
+            else
+                return countX(str.substring(1));
+
 
         }
 
