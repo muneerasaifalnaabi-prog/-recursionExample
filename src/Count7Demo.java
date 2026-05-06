e6567185ee43fb63035d0267d3b71fc6059e029c
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-05-06 15:59:56 +0400

    called count7 method using object and test it

--- src/Count7Demo.java
+++ src/Count7Demo.java
@@ -2,6 +2,8 @@ public class Count7Demo {
     public static void main(String[] args) {
         Count7 count7=new Count7();
         System.out.println(count7.count7(717));
+        System.out.println(count7.count7(7));
+        System.out.println(count7.count7(123));
 
 
     }
