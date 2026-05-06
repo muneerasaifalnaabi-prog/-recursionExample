aded367590669fa7ac82336df5ddaf9b260f788a
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-05-06 16:32:20 +0400

    called method with added test case

--- src/Array11Demo.java
+++ src/Array11Demo.java
@@ -3,7 +3,11 @@ public class Array11Demo {
         //created object
         Array11 array11 = new Array11();
         int[] arr={1, 2, 11};
+        int[] arr1={11, 11};
+        int[] arr2={1, 2, 3, 4};
 
+        System.out.println(array11.array11(arr, 0));
+        System.out.println(array11.array11(arr, 0));
         System.out.println(array11.array11(arr, 0));
 
 
