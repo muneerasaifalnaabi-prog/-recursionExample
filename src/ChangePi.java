7edb47b84a9fdef956b05148006f368a37adfdbe
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-05-06 16:13:53 +0400

    Added Base Case

--- src/ChangePi.java
+++ src/ChangePi.java
@@ -1,6 +1,10 @@
 public class ChangePi {
     public  String  changePi(String str){
         //Base Case
+        if  (str.length() == 0){
+            return "";
+        }
+        //Recursive Case
 
     }
 }
