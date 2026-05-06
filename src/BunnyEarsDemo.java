8db7aa20a966e1fa75e1218efeae142bd11e982f
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-05-06 15:43:15 +0400

    Called  the bunnyEars2() method using the object

--- src/BunnyEarsDemo.java
+++ src/BunnyEarsDemo.java
@@ -2,6 +2,8 @@ public class BunnyEarsDemo {
     public static void main(String[] args) {
         BunnyEars bunnyEars= new BunnyEars();
         System.out.println(bunnyEars.bunnyEars2(0));
+        System.out.println(bunnyEars.bunnyEars2(1));
+        System.out.println(bunnyEars.bunnyEars2(2));
 
 
     }
