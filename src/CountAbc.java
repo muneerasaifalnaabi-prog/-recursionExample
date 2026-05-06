f62e9c7db5276a62b2027e177555bf9e1842de4b
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-05-06 17:13:31 +0400

    Completed Task 8 :CountAbc

--- src/CountAbc.java
+++ src/CountAbc.java
@@ -1,16 +1,17 @@
 public class CountAbc {
-  public int countAbc(String str){
-      int count =0;
-      //Base Case
-      if (str.length()<3){
-          return 0;
-      }
-      else {
-          //Recursive Case
-          if (str.substring(0,3)=="abc" ||str.substring(0,3)=="aba" ){
-              return 1 +countAbc(str.substring(1));
-          }
-          return countAbc(str.substring(1));
-      }
-  }
+
+    public int countAbc(String str) {
+
+        if (str.length() < 3) {
+            return 0;
+        }
+
+        String first3 = str.substring(0, 3);
+
+        if (first3.equals("abc") || first3.equals("aba")) {
+            return 1 + countAbc(str.substring(1));
+        }
+
+        return countAbc(str.substring(1));
+    }
 }
