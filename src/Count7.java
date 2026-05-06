b2bbcefb272a66bd6628516e5daeb45194c34c3c
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-05-06 15:48:57 +0400

    Added lastDigit &remaining Digit

--- src/Count7.java
+++ src/Count7.java
@@ -1,7 +1,13 @@
 public class Count7 {
     public int count7(int n){
+        //Base case
         if (n==0){
             return 0;
         }
+        //Recursive Case
+        int lastDigit = n % 10;
+        int remainig =n/10;
+
+
     }
 }
