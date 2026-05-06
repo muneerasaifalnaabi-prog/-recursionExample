f74b18eeb3539d06feb66a56775caef22be5d810
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-05-06 16:22:22 +0400

    Completed task 5 :ChangePi

--- src/ChangePiDemo.java
+++ src/ChangePiDemo.java
@@ -2,6 +2,7 @@ public class ChangePiDemo {
     public static void main(String[] args) {
         //created object for ChangePi
         ChangePi changePi= new ChangePi();
+        //tested case
         System.out.println(changePi.changePi("xpix"));
         System.out.println(changePi.changePi("pipi"));
         System.out.println(changePi.changePi("pip"));
