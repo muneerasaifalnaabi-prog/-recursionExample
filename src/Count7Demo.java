c1b228554c3064e82b1ab77666d102f6e4a8cd68
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-05-06 16:01:11 +0400

    Completed Task 3 :Count7

--- src/Count7Demo.java
+++ src/Count7Demo.java
@@ -1,6 +1,8 @@
 public class Count7Demo {
     public static void main(String[] args) {
+        //create object
         Count7 count7=new Count7();
+        //call count7 method
         System.out.println(count7.count7(717));
         System.out.println(count7.count7(7));
         System.out.println(count7.count7(123));
