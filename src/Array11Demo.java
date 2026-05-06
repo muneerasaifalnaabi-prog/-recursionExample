8d320cb329528d581cabd6aef4861a5ff23d2d45
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-05-06 16:33:23 +0400

    Completed Task 6 : Array11

--- src/Array11Demo.java
+++ src/Array11Demo.java
@@ -5,7 +5,7 @@ public class Array11Demo {
         int[] arr={1, 2, 11};
         int[] arr1={11, 11};
         int[] arr2={1, 2, 3, 4};
-
+        //test case
         System.out.println(array11.array11(arr, 0));
         System.out.println(array11.array11(arr1, 0));
         System.out.println(array11.array11(arr2, 0));
