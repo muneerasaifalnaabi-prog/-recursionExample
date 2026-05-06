813904060c52a6bea30e75a2f7ab54a8d5964f0b
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-05-06 16:20:36 +0400

    called method using object and tested

--- src/ChangePiDemo.java
+++ src/ChangePiDemo.java
@@ -2,6 +2,7 @@ public class ChangePiDemo {
     public static void main(String[] args) {
         //created object for ChangePi
         ChangePi changePi= new ChangePi();
+        System.out.println(changePi.changePi("xpix"));
 
 
     }
