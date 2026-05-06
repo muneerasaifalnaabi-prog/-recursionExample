b81cd45451552046b49b2786bec42da0fd1d4f91
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-05-06 16:46:14 +0400

    Called method by object and added test case

--- src/PairStarDemo.java
+++ src/PairStarDemo.java
@@ -2,5 +2,8 @@ public class PairStarDemo {
     public static void main(String[] args) {
         //createde object
         PairStar pairStar= new PairStar();
+        System.out.println(pairStar.pairStar("hello") );
+        System.out.println(pairStar.pairStar("xxyy") );
+        System.out.println(pairStar.pairStar("aaaa") );
     }
 }
