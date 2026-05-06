1193b89260159082d207f869ba19ff01b2799b49
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-05-06 16:50:56 +0400

    Completed Task 7 :PairStar

--- src/PairStarDemo.java
+++ src/PairStarDemo.java
@@ -2,6 +2,7 @@ public class PairStarDemo {
     public static void main(String[] args) {
         //createde object
         PairStar pairStar= new PairStar();
+        //test case
         System.out.println(pairStar.pairStar("hello") );
         System.out.println(pairStar.pairStar("xxyy") );
         System.out.println(pairStar.pairStar("aaaa") );
