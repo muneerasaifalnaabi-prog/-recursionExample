1351a9472839dca33a20f36947cdd24dade1307f
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-05-06 15:31:07 +0400

    added   Test the method with values

--- src/FactorialDemo.java
+++ src/FactorialDemo.java
@@ -2,7 +2,7 @@ public class FactorialDemo {
     public static void main(String[] args) {
         Factorial fact =new Factorial();
         System.out.println(fact.factorial(5));
-        fact.factorial(3);
+        System.out.println(fact.factorial(3)); ;
         fact.factorial(10);
 
 
