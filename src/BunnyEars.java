2c818a64e817126e6aeb2497c3ff1eb1148fe725
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-05-06 15:37:49 +0400

    added BunnyEarsDemo   class

--- src/BunnyEars.java
+++ src/BunnyEars.java
@@ -8,11 +8,9 @@ public class BunnyEars {
         else {
             if (n%2==0){
                 return 3+bunnyEars2(n-1);
-
             }
             else
                 return 2+bunnyEars2(n-1);
         }
-
     }
 }
