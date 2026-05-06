5eafdcfd291a2ed7fc356797fd9974f1617bbcc7
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-05-06 16:09:51 +0400

    Tested case with calling method

--- src/CountXDemo.java
+++ src/CountXDemo.java
@@ -2,6 +2,7 @@ public class CountXDemo {
     public static void main(String[] args) {
         //Created object  of Countx class
         CountX countX=new CountX();
+        System.out.println(countX.countX("xxhixx") );
 
     }
 }
