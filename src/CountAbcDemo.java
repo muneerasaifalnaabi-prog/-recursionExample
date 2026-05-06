2cee82267d8a01fc107fa8bd0b3df002251e3f2d
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-05-06 17:09:20 +0400

    Completed Task 8 :CountAbc

--- src/CountAbcDemo.java
+++ src/CountAbcDemo.java
@@ -2,7 +2,12 @@ public class CountAbcDemo {
     public static void main(String[] args) {
         //Created object
         CountAbc countAbc= new CountAbc();
+// testy case
+        System.out.println(countAbc.countAbc("abc"));
+        System.out.println(countAbc.countAbc("abcxxabc"));
+        System.out.println(countAbc.countAbc("abaxxaba"));
 
 
     }
 }
+
