386fe9543007ca9d4e6ae81854c3363a9fdd7749
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-05-06 16:10:53 +0400

    Completed Task 4 : CountX

--- src/CountXDemo.java
+++ src/CountXDemo.java
@@ -2,6 +2,7 @@ public class CountXDemo {
     public static void main(String[] args) {
         //Created object  of Countx class
         CountX countX=new CountX();
+        //tested case
         System.out.println(countX.countX("xxhixx") );
         System.out.println(countX.countX("xhixhix") );
         System.out.println(countX.countX("hi") );
