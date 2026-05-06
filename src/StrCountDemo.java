51d208a958701b0f6593f72c6d5525c8453ca16b
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-05-06 17:31:16 +0400

    Completed Task 9 : StrCount

--- src/StrCountDemo.java
+++ src/StrCountDemo.java
@@ -1,3 +1,10 @@
 public class StrCountDemo {
+    public static void main(String[] args) {
+        StrCount strCount=new StrCount();
+        System.out.println(strCount.strCount("catcowcat", "cat"));
+        System.out.println(strCount.strCount("catcowcat", "cow"));
+        System.out.println(strCount.strCount("catcowcat", "dog"));
+
+    }
 
 }
