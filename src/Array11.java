46c70dd7afeab3aafbc8d410f660ec63d5b10300
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-05-06 16:24:55 +0400

    Added Base case

--- src/Array11.java
+++ src/Array11.java
@@ -1,7 +1,9 @@
 public class Array11 {
     public int  array11(int[] nums, int index){
         //Base Case
-        if ( index == nums.length)
+        if ( index == nums.length){
+            return 0;
+        }
 
 
     }
