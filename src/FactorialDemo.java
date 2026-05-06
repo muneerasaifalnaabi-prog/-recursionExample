4e4e3e86cc93705bfe99ee0cab5033f86f0eabc1
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-05-06 15:29:09 +0400

    added   Test the method with values

--- src/FactorialDemo.java
+++ src/FactorialDemo.java
@@ -2,6 +2,8 @@ public class FactorialDemo {
     public static void main(String[] args) {
         Factorial fact =new Factorial();
         fact.factorial(5);
+        fact.factorial(3);
+        fact.factorial(10);
 
 
 
