356bfa28031a0536f4a889694a26254213dd8059
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-05-07 08:50:33 +0400

    Called method using object and added test case

--- src/Count8Demo.java
+++ src/Count8Demo.java
@@ -1,7 +1,10 @@
 public class Count8Demo {
     public static void main(String[] args) {
        //Create object
-
+       Count8 count8= new Count8();
+        System.out.println(count8.count8(8));
+        System.out.println(count8.count8(818));
+        System.out.println(count8.count8(8818));
 
     }
 }
