431c83e78b5e49cff526d7de6c8343e10eb9d2f3
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-05-07 08:00:37 +0400

    Called method using object and added tested case

--- src/BunnyEarsDemo1.java
+++ src/BunnyEarsDemo1.java
@@ -2,6 +2,9 @@ public class BunnyEarsDemo1 {
     public static void main(String[] args) {
         //Created object
         BunnyEars1 bunnyEars1=new BunnyEars1();
+        System.out.println(bunnyEars1.bunnyEars(0));
+        System.out.println(bunnyEars1.bunnyEars(1));
+        System.out.println(bunnyEars1.bunnyEars(2));
 
     }
 }
