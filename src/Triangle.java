61f7b8f0e09847598d9d0d60a84f76ad332b15a1
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-05-07 08:35:21 +0400

    Completed Task 11 : Triangle

--- src/Triangle.java
+++ src/Triangle.java
@@ -1,7 +1,9 @@
 public class Triangle {
   public  int  triangle(int n) {
       //Base Case
-      if (n==0) return 0;
+      if (n==0) {
+          return 0;
+      }
       //Recursive Case
       return  n + triangle(n - 1);
   }
--- src/TriangleDemo.java
+++ src/TriangleDemo.java
@@ -5,7 +5,7 @@ public class TriangleDemo {
 
         System.out.println(triangle.triangle(0));
         System.out.println(triangle.triangle(1));
-        System.out.println(triangle.triangle(3));
+        System.out.println(triangle.triangle(2));
 
 
     }
