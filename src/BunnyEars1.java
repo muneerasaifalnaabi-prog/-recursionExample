37b61f0e4ee4fca99db58c2ac5c0216668133f3a
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-05-07 07:58:06 +0400

    Added class BunnyEarsDemo1

--- src/BunnyEars1.java
+++ src/BunnyEars1.java
@@ -1,16 +1,15 @@
 public class BunnyEars1 {
-    public int bunnyEars(int n){
+    public int bunnyEars(int n) {
         //Base Case
-        if (n==0){
+        if (n == 0) {
             return 0;
         }
         //Recursive Case
-        if (n==2){
-            return  2 + bunnyEars(n - 1) ;
-
+        else {
+            if (n == 2) {
+                return 2 + bunnyEars(n - 1);
+            }
 
         }
-
     }
-
 }
